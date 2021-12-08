01f6aae570a61e7bcec76be1a064d8d79ca69a1f
Noa Udler <udlernoa4@gmail.com>
2021-12-08 14:37:28 -0800

    adding favorites

---
+++ src/components/Favorites.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+function Favorites() {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+export default Favorites
---
+++ src/components/MovieDetail.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+
+function MovieDetail({ movies }) {
+  const params = useParams();
+  console.log(params);
+  const movie = movies.find((movie) => movie.id === parseInt(params.movieId));
+  console.log(movie);
+
+  if (!movie) {
+    return null;
+  }
+  return (
+    <div className="movie-details">
+      <div className="col">
+        <div className="card shadow-sm">
+          <img src={movie["movie-image"]} alt={movie.title} />
+
+          <div className="card-body">
+            <p className="card-text">{movie.title}</p>
+            <p className="card-text">{movie.description}</p>
+            <p className="card-text">{movie["running-time"]}</p>
+            <div className="d-flex justify-content-between align-items-center">
+                <Link to="/favorites">
+              <div className="btn-group">
+                <button type="button" className="btn btn-sm btn-primary">
+                  Add To Favorites!
+                </button>
+              </div>
+              </Link>
+              <Link to="/movie-list">
+              <div className="btn-group">
+                <button type="button" className="btn btn-sm btn-primary">
+                  Back to Movie List
+                </button>
+              </div>
+              </Link>
+              <small className="text-danger">⭐{movie.score}</small>
+              <small className="text-danger">{movie.rating}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieDetail;
